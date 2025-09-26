6038198b0bb09e2b92d2f41375651f5a3be0ff18
omkarp <Pawaromkar@030gmail.com>
2025-09-26 21:08:31 +0530

    first commit

---
+++ Login/Dolist/Task 4/script.js
@@ -0,0 +1,7 @@
+// Toggle mobile navigation
+const menuBtn = document.getElementById("menuBtn");
+const menu = document.getElementById("menu");
+
+menuBtn.addEventListener("click", () => {
+  menu.style.display = menu.style.display === "flex" ? "none" : "flex";
+});
